27e6a2b75f2f5efb1eb334b7c98b38fcdcc663c7
Ana Carolina Maria <anasantin31@gmail.com>
2026-03-02 21:17:58 -0300

    teste de commit apenas

---
+++ trabalhoM1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "lista_duplamente_encadeada.hpp"
+using namespece std;
+
+//Trabalho M1 - Estrutura de Dados - (Mateus Benvenutti, Ana Carolina MAria, Thiago Filisbino)
+
